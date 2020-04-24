08326d40254b7b335f2b737092f62913e99e254c
n-wagner <wagner.nick@live.com>
2020-04-23 23:29:05 -0400

    Food Items poster also has their phone number recorded so they can be chatted, open donations and receipients pages ready for button logic

--- lib/screens/mainmenu/waiting_pickup/waiting_pickup.dart
+++ lib/screens/mainmenu/waiting_pickup/waiting_pickup.dart
@@ -38,7 +38,7 @@ class _WaitingPickupState extends State<WaitingPickup>
         print("User " + user.toString());
         if (item.uid == null || item.swipers == null) throw new FormatException("item uid or swipers map was found null", item);
         // Keep only things you've swiped on that are still active
-        if (item.closed == false && item.swipers.containsKey(user.uid)) {  // if (!user.matches.contains(item.docID)) {
+        if (item.closed == false && item.uid[0] == user.uid) {  // if (!user.matches.contains(item.docID)) {
           return false;
         } else {
           return true;
@@ -65,7 +65,28 @@ class _WaitingPickupState extends State<WaitingPickup>
         child: Center(
           child: ListView(
             children: foodItems == null ? [Text("Loading...")]: foodItems.length == 0 ? [Text("No orders waiting to be picked up!")] : foodItems.map((FoodItem foodItem) {
-              return FoodItemCard(foodItem: foodItem);
+              return Column(
+                children: <Widget>[
+                  FoodItemCard(foodItem: foodItem),
+                  Row(
+                    children: <Widget>[
+                      RaisedButton(
+                        onPressed: () {
+                          //TODO: Take me to a page where I can do drop downs with the phone numbers of the people
+                          List<String> swipers = foodItem.swipers.values.toList();
+                        },
+                        child: Text("Accept"),
+                      ),
+                      RaisedButton(
+                        onPressed: foodItem.accepted == null || foodItem.accepted == '' ? null : () {
+                          //_db.setClosedForFoodItem(foodID: foodItem.docID)
+                        },
+                        child: Text("Close Order"),
+                      )
+                    ],
+                  ),
+                ]
+              );
             }).toList(),
           ),
         ),
