ec66f1cb8ee4ed6f14c85761f32052d7a66d7c9f
n-wagner <wagner.nick@live.com>
2020-04-24 00:56:59 -0400

    Food Items not storing properly without reference to full user for database (needed a wrapper page) and added an acceptance choice page

---
+++ lib/screens/givefood/new_food_page_wrapper.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:food_savior/models/user.dart';
+import 'package:food_savior/screens/givefood/new_food_page.dart';
+import 'package:food_savior/services/database.dart';
+import 'package:provider/provider.dart';
+
+class NewFoodPageWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final user = Provider.of<User>(context);
+    
+    if (user == null) {
+      return Text("Loading...");
+      // StreamProvider<List<FoodItem>>.value(
+      //   value: DatabaseService().foodItems,
+      //   child: PastOrders(),
+      // );
+    } else {
+      return StreamProvider<User>.value(
+        value: DatabaseService(uid: user.uid).user,
+        child: NewFoodPage(),
+      );
+    }
+  }
+}
